4c528cc486ea9462cd58367f4861686fc12e3956
Talabér Imre <talaber.imre>
2026-05-19 12:37:42 +0200

    Nextjs intall es cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,8 @@ import Alert from './alert';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Alert type="success" title="Success!" message="Your action was ok." />
-      </main>
+    <div>
+        <h1>Fejlesztés alatt</h1>
     </div>
   );
 }
