7ea5054729fb4f3b352f3b06b6680de703285e47
Talabér Imre <talaber.imre>
2026-05-19 13:12:39 +0200

    Nextjs intall es cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import Image from "next/image";
-import Alert from './alert';
-
-
+'use client';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 export default function Home() {
   return (
-    <div>
-        <h1>Fejlesztés alatt</h1>
+    <div className='modal show' style={{ display: 'block', position: 'initial' }}>
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary'>Close</Button>
+          <Button variant='primary'>Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
     </div>
   );
 }
